cc28bd82a9fb7c500b9d156957c9cd62778c3e99
Tyler Robinson <tyler.robinson.3@gmail.com>
2016-07-06 09:53:29 -0600

    commit Thread Controller

--- Thread Controller.swift
+++ Thread Controller.swift
@@ -13,13 +13,35 @@ class ThreadController {
     
     static let sharedController = ThreadController()
     
-    var messages: [Message] = []
+    var cloudKitManager = CloudKitManager()
+    
+    var isSyncing: Bool = false
+    
+    var messages: [Message] {
+        let fetchRequest = NSFetchRequest(entityName: Message.typeKey)
+        let sortDescriptor = NSSortDescriptor(key: Message.timestampKey, ascending: false)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        let results = (try? Stack.sharedStack.managedObjectContext.executeRequest(fetchRequest)) as? [Message] ?? []
+        
+        return results
+    }
+    
+    init () {
+        
+        self.cloudKitManager = CloudKitManager()
+        
+        performFullSync()
+        
+        
+    }
     
     //MARK: - Thread -
     
     func createThread() {
         
     }
+    
     func deleteThread() {
         
     }
@@ -34,32 +56,127 @@ class ThreadController {
     
     // MARK: Syncable Records -
     
-    func syncedRecords() {
+    func syncedRecords(type: String) -> [CloudKitManagedObject] { // this just shows you which records are synced between cloudkit and core data
+        let fetchRequest = NSFetchRequest(entityName: type)
+        let predicate = NSPredicate(format: "recordID != nil")
         
+        fetchRequest.predicate = predicate
+        
+        let results = (try? Stack.sharedStack.managedObjectContext.executeRequest(fetchRequest)) as? [CloudKitManagedObject] ?? []
+        
+        return results
     }
     
-    func unsyncedRecords() {
+    func unsyncedRecords(type: String) -> [CloudKitManagedObject] { // this just shows you which records are unsynced between cloudkit and core data
+        let fetchRequest = NSFetchRequest(entityName: type)
+        let predicate = NSPredicate(format: "recordID == nil")
         
+        fetchRequest.predicate = predicate
+        
+        let results = (try? Stack.sharedStack.managedObjectContext.executeRequest(fetchRequest)) as? [CloudKitManagedObject] ?? []
+        
+        return results
     }
     
-    func performFullSync() {
+    func performFullSync(completion: (() -> Void)? = nil) { // this syncs the records between coredata and cloudkit
         
+        if isSyncing {
+            if let completion = completion {
+                completion()
+            }
+        } else {
+            isSyncing = true
+            
+            pushChangesToCloudKit { (success, error) in
+                
+                self.fetchNewRecords(Message.typeKey) {
+                    
+                    self.fetchNewRecords(Thread.typeKey, completion: {
+                        
+                        self.isSyncing = false
+                        
+                        if let completion = completion {
+                            
+                            completion()
+                        }
+                    })
+                }
+            }
+        }
     }
     
     // MARK: Fetch Records -
     
-    func fetchNewRecords() {
+    func fetchNewRecords(type: String, completion: (() -> Void)?) {
         
+        let referencesToExclude = syncedRecords(type).flatMap({ $0.cloudKitReference})
+        var prediate = NSPredicate(format: "NOT(recordID IN %@", argumentArray: [referencesToExclude])
+        
+        if referencesToExclude.isEmpty {
+            prediate = NSPredicate(value: true)
+        }
+        
+        cloudKitManager.fetchRecordsWithType(type, predicate: prediate, recordFetchedBlock: { (record) in
+            
+            switch type {
+                
+            case Thread.typeKey:
+                let _ = Thread(record: record)
+                
+            case Message.typeKey:
+                let _ = Message(record: record)
+                
+            default:
+                return
+            }
+            
+            self.saveContext()
+            
+        }) { (records, error) in
+            
+            if error != nil {
+                print("Unable to fetch new records")
+            }
+            
+            if let completion = completion {
+                completion()
+            }
+        }
     }
     
-    func pushChangesToCloudKit() {
+    func pushChangesToCloudKit(completion: ((success: Bool, error: NSError?) -> Void)?) {
         
+        let unsavedManagedObjects = unsyncedRecords(Message.typeKey) + unsyncedRecords(Thread.typeKey)
+        let unsavedRecords = unsavedManagedObjects.flatMap({ $0.cloudKitRecord })
+        
+        cloudKitManager.saveRecords(unsavedRecords, perRecordCompletion: { (record, error) in
+            
+            guard let record = record else { return }
+            
+            if let matchingRecord = unsavedManagedObjects.filter({ $0.recordName == record.recordID.recordName }).first {
+                
+                matchingRecord.update(record)
+            }
+            
+        }) { (records, error) in
+            
+            if let completion = completion {
+                
+                let success = records != nil
+                completion(success: success, error: error)
+            }
+        }
     }
     
     // MARK: Save Content -
     
-    func saveContent() {
-
+    func saveContext() {
+        
+        do {
+            try Stack.sharedStack.managedObjectContext.save()
+        } catch {
+            print("Unable to save context. \(error)")
+        }
     }
 }
 
--- TravChat/CloudKitManagedObject.swift
+++ TravChat/CloudKitManagedObject.swift
@@ -10,7 +10,52 @@ import Foundation
 import CloudKit
 import CoreData
 
-
 @objc protocol CloudKitManagedObject {
     
+    var timestamp: NSDate { get set }
+    var recordID: NSData? { get set }
+    var recordName: String { get set }
+    var recordType: String { get }
+    var cloudKitRecord: CKRecord? { get }
+    
+    init?(record: CKRecord, context: NSManagedObjectContext)
+}
+
+extension CloudKitManagedObject {
+    
+    var isSynced: Bool {
+        
+        return recordID != nil
+    }
+    
+    var cloudKitRecordID: CKRecordID? {
+       
+        guard let recordID = recordID,
+            let recordIDData = NSKeyedUnarchiver.unarchiveObjectWithData(recordID) as? CKRecordID else { return nil }
+        
+        return recordIDData
+    }
+    
+    var cloudKitReference: CKReference? {
+        
+        guard let recordID = cloudKitRecordID else { return nil }
+        
+        return CKReference(recordID: recordID, action: .None)
+    }
+    
+    func update(record: CKRecord) {
+        
+        self.recordID = NSKeyedArchiver.archivedDataWithRootObject(record.recordID)
+        
+        do {
+            try? Stack.sharedStack.managedObjectContext.save()
+        } catch {
+            print("Unable to save Managed Object Context: \(error)")
+        }
+    }
+    
+    func nameForManagedObject() -> String {
+        
+        return NSUUID().UUIDString
+    }
 }
